df8a97bcfc9c2e28a564b6a50ce1f24c5e032c7c
Kenny Egun <kennyegun240@gmail.com>
2023-04-20 20:21:23 +0100

    user and all users redux

---
+++ src/redux/allUsers/allusers.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllUsers = createAsyncThunk('getUser/getAllUsers', async (TOKEN) => {
+    const BASE_URL = 'http://localhost:3000/api/v1'
+    const userRequest = axios.create({
+        baseURL: BASE_URL,
+        headers: { 'Authorization': `Bearer ${TOKEN}` }
+    })
+    const get = await userRequest.get('admin/all_users')
+    delete get.headers
+    return get.data
+})
+
+const allUsersSlice = createSlice({
+    name: 'allUsers',
+    initialState: {
+        allUsers: [],
+        status: null
+    },
+    reducers: {},
+    extraReducers(reducer) {
+        reducer
+            .addCase(getAllUsers.pending, (state) => {
+                const isFulfilled = state;
+                isFulfilled.status = 'Pending';
+            })
+            .addCase(getAllUsers.fulfilled, (state, action) => {
+                const isFulfilled = state;
+                isFulfilled.status = 'Fulfilled';
+                isFulfilled.allUsers = action.payload;
+            })
+    }
+})
+
+export default allUsersSlice.reducer
