baa02ede570a83c1ae98e646c100bbaba323e510
Kenny Egun <kennyegun240@gmail.com>
2023-04-20 20:21:54 +0100

    redux storre

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userSlice from './user/user'
+import allUsersSlice from './allUsers/allusers'
+// import storeSlice from './store/store'
+// import orderSlice from './order/order'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, userSlice)
+
+export const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+    allUsers: allUsersSlice
+    // store: storeSlice,
+    // order: orderSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+export const persistor = persistStore(store)
