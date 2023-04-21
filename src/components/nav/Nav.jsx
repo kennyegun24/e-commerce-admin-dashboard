fbda0eb380aec3b7086bd7c1e1b30ef8187c5767
Kenny Egun <kennyegun240@gmail.com>
2023-04-21 18:47:31 +0100

    Moodify and dispatch all fetch functions

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginSuccess } from '../../redux/user/user'
-import { FaStore, FaCheck, FaChartBar } from 'react-icons/fa'
-// import { fetchOrders } from '../../redux/order/order'
-// import { getStore } from '../../redux/store/store'
+import { FaStore, FaCheck, FaChartBar, FaUser, FaPlus, FaPlusCircle } from 'react-icons/fa'
+import { getAllUsers } from '../../redux/allUsers/allusers'
+import { getAllOrders } from '../../redux/allOrders/allOrders'
+import { getAllProducts } from '../../redux/allProducts/allProducts'
+import { getAllStores } from '../../redux/allStores/allStores'
 
 const Nav = () => {
   const { currentUser } = useSelector((state) => state.user)
@@ -15,24 +17,25 @@ const Nav = () => {
   }
 
   useEffect(() => {
-    // dispatch(fetchOrders(currentUser.data.token))
-    // dispatch(getStore(currentUser.data.token))
-    console.log('orders')
+    dispatch(getAllUsers(currentUser.data.token))
+    dispatch(getAllOrders(currentUser.data.token))
+    dispatch(getAllProducts(currentUser.data.token))
+    dispatch(getAllStores(currentUser.data.token))
   }, [])
-  console.log('asasaas')
 
   return (
     <>
       <nav className='mainNavHeader'>
         <div className='navOptions'>
           <ul>
-            <div>
-              {/* <img className='navImage' src={currentUser.store.image} alt="" /> */}
-            </div>
-
             <NavLink to='/' className='navLink'><FaChartBar /> Statistics</NavLink>
             <NavLink className='navLink' to='/products'><FaStore /> Products</NavLink>
             <NavLink className='navLink' to='/orders'><FaCheck /> Orders</NavLink>
+            <NavLink className='navLink' to='/users'><FaUser /> Users</NavLink>
+            <NavLink className='navLink' to='/stores'><FaStore /> Stores</NavLink>
+            <NavLink className='navLink' to='/add/store'><FaPlus /> Add Store</NavLink>
+            <NavLink className='navLink' to='/add/category'><FaPlusCircle /> Add Category</NavLink>
+            <NavLink className='navLink' to='/add/product'><FaPlusCircle /> Add Product</NavLink>
             <button className='logoutButton' onClick={() => logout()}>Logout</button>
           </ul>
         </div>
