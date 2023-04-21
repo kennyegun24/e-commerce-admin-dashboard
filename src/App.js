a842c8b4a60469160c3e8bf1ccc8e7c9f6769017
Kenny Egun <kennyegun240@gmail.com>
2023-04-21 18:53:52 +0100

    Modify

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Register from './components/auth/Register';
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import Nav from './components/nav/Nav';
-// import Home from './components/pages/Home';
 import NavHead from './components/nav/NavHead';
 import HomePage from './components/pages/HomePage';
-// import Table from './components/pages/Table';
-// import Orders from './components/pages/Orders';
-
+import AllProducts from './components/pages/AllProducts';
+import AllOrders from './components/pages/AllOrders';
+import AllUsers from './components/pages/AllUsers';
+import AllStores from './components/pages/AllStores';
+import AddCategory from './components/forms/AddCategory';
+import AddStore from './components/forms/AddStore';
+import AddProduct from './components/forms/AddProduct';
 
 const App = () => {
   const { currentUser } = useSelector(state => state.user)
@@ -31,8 +34,13 @@ const App = () => {
           <Routes>
             <Route path='/register' element={currentUser ? <Navigate to='/' /> : <Register />} />
             <Route path='/' element={currentUser ? <HomePage /> : <Login />} />
-            {/* <Route path='/products' element={currentUser ? <Table /> : <Navigate to='/' />} />
-            <Route path='/orders' element={currentUser ? <Orders /> : <Navigate to='/' />} /> */}
+            <Route path='/products' element={currentUser ? <AllProducts /> : <Navigate to='/' />} />
+            <Route path='/orders' element={currentUser ? <AllOrders /> : <Navigate to='/' />} />
+            <Route path='/users' element={currentUser ? <AllUsers /> : <Navigate to='/' />} />
+            <Route path='/stores' element={currentUser ? <AllStores /> : <Navigate to='/' />} />
+            <Route path='/add/category' element={currentUser ? <AddCategory /> : <Navigate to='/' />} />
+            <Route path='/add/store' element={currentUser ? <AddStore /> : <Navigate to='/' />} />
+            <Route path='/add/product' element={currentUser ? <AddProduct /> : <Navigate to='/' />} />
           </Routes>
         </div>
       </div>
