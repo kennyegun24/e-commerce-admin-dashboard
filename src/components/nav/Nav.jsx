5858c209058eee78471625748335ea371bf4e1bc
Kenny Egun <kennyegun240@gmail.com>
2023-04-22 20:42:19 +0100

    Modify styling for mobile view

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginSuccess } from '../../redux/user/user'
-import { FaStore, FaCheck, FaChartBar, FaUser, FaPlus, FaPlusCircle } from 'react-icons/fa'
+import { FaStore, FaCheck, FaChartBar, FaUser, FaPlus, FaPlusCircle, FaBars, FaToggleOff, FaWindowClose } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 import { getAllUsers } from '../../redux/allUsers/allusers'
 import { getAllOrders } from '../../redux/allOrders/allOrders'
 import { getAllProducts } from '../../redux/allProducts/allProducts'
@@ -10,6 +11,7 @@ import { getAllStores } from '../../redux/allStores/allStores'
 
 const Nav = () => {
   const { currentUser } = useSelector((state) => state.user)
+  const [toggle, setToggle] = useState(false)
 
   const dispatch = useDispatch()
   const logout = () => {
@@ -23,23 +25,39 @@ const Nav = () => {
     dispatch(getAllStores(currentUser.data.token))
   }, [])
 
+  const toggleOn = () => {
+    if (!toggle) {
+      setToggle(true)
+    } else setToggle(false)
+  }
+
+  const hideMenu = () => {
+    setToggle(false)
+  }
+
   return (
     <>
-      <nav className='mainNavHeader'>
-        <div className='navOptions'>
-          <ul>
-            <NavLink to='/' className='navLink'><FaChartBar /> Statistics</NavLink>
-            <NavLink className='navLink' to='/products'><FaStore /> Products</NavLink>
-            <NavLink className='navLink' to='/orders'><FaCheck /> Orders</NavLink>
-            <NavLink className='navLink' to='/users'><FaUser /> Users</NavLink>
-            <NavLink className='navLink' to='/stores'><FaStore /> Stores</NavLink>
-            <NavLink className='navLink' to='/add/store'><FaPlus /> Add Store</NavLink>
-            <NavLink className='navLink' to='/add/category'><FaPlusCircle /> Add Category</NavLink>
-            <NavLink className='navLink' to='/add/product'><FaPlusCircle /> Add Product</NavLink>
-            <button className='logoutButton' onClick={() => logout()}>Logout</button>
-          </ul>
+      <div>
+        <div className='bar'>
+          {!toggle && <FaBars onClick={toggleOn} />}
+          {toggle && <AiOutlineClose onClick={toggleOn} />}
         </div>
-      </nav>
+        <nav className={`mainNavHeader ${toggle ? '' : 'hide'}`}>
+          <div className='navOptions'>
+            <ul className='navUls'>
+              <NavLink onClick={hideMenu} to='/' className='navLink'><FaChartBar /> Statistics</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/products'><FaStore /> Products</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/orders'><FaCheck /> Orders</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/users'><FaUser /> Users</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/stores'><FaStore /> Stores</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/add/store'><FaPlus /> Add Store</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/add/category'><FaPlusCircle /> Add Category</NavLink>
+              <NavLink onClick={hideMenu} className='navLink' to='/add/product'><FaPlusCircle /> Add Product</NavLink>
+              <button className='logoutButton' onClick={() => logout()}>Logout</button>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
